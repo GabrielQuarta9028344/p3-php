3aa8f0dfeeaf30e40d88cb14323d617c13a92f7e
GabrielQuarta9028344 <9028344@student.zadkine.nl>
2026-03-06 10:53:02 +0100

    opdracht 1 en 2

---
+++ opdracht1/shorthand.php
@@ -0,0 +1,21 @@
+<?php
+    $naam = "Gabriel";
+    $leeftijd = 17;
+    $favorieteTaal = "PHP";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Shorthand voorbeeld</title>
+</head>
+<body>
+
+    <h1>Mijn gegevens</h1>
+
+    <p>Naam: <?= $naam ?></p>
+    <p>Leeftijd: <?= $leeftijd ?></p>
+    <p>Favoriete programmeertaal: <?= $favorieteTaal ?></p>
+
+</body>
+</html>
---
+++ opdracht2/if-statement.php
@@ -0,0 +1,25 @@
+<?php
+
+$ingelogd = true;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p> <?php
+    if ($ingelogd === true) {
+    echo "Welkom terug!";
+} else {
+    echo "Log eerst in.";
+}
+
+?> </p>
+</body>
+</html>
